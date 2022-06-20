ce150df802b13b1ec430e21deef98487ba9f5046
yosefHesham <yousefmobdev@gmail.com>
2022-06-21 01:15:54 +0200

    Configure tore

---
+++ src/redux/configure_store.js
@@ -0,0 +1,11 @@
+
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import missionsReducer from './missions/missions';
+
+const mainReducer = combineReducers({
+  missions:missionsReducer
+})
+
+const store = createStore(mainReducer,applyMiddleware(thunk))
