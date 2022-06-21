5493c113bb25db4922d7ccf9a00d98a851607b07
yosefHesham <yousefmobdev@gmail.com>
2022-06-21 23:14:38 +0200

    Fix rocketsS

--- src/components/routeHandler.jsx
+++ src/components/routeHandler.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Missions from './missions/missions';
 import Profile from './profile/profile';
 import Navbar from './nav_bar';
+import Rockets from './rockets/rockets';
 
 const RouteHandler = () => (
+
   <Router>
     <Navbar />
     <Routes>
-      {/* <Route path="/" element={<Rockets />} /> */}
+      <Route path="/" element={<Rockets />} />
       <Route path="/missions" element={<Missions />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
--- src/redux/configure_store.js
+++ src/redux/configure_store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import missionsReducer from './missions/missions';
+import rocketsReducer from './rockets/rockets';
 
 const mainReducer = combineReducers({
   missions: missionsReducer,
+  rockets:rocketsReducer
 });
 
 const store = createStore(mainReducer, applyMiddleware(thunk));
