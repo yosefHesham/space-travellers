135d02b2221ddb3cd9c1ba0a18c9d6ab707799a1
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-06-23 17:19:56 +0100

    Fix key id needed

--- src/components/profile/joinedMissions.jsx
+++ src/components/profile/joinedMissions.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable max-len */
 import React from 'react';
 import { useSelector } from 'react-redux';
-import './Profile.css';
 
 const Joined = () => {
   const missions = useSelector((state) => state.missions);
@@ -13,23 +11,26 @@ const Joined = () => {
   };
   const table = {
     width: '48%',
-    height: '40px',
+    height: '50px',
     borderCollapse: 'collapse',
+    border: '1px solid #AAAAAA',
   };
   return (
     <div className="profile_container" style={reserveMissionContainer}>
-      <span style={{ margin: '20px 0' }}>
+      <span style={{ margin: '20px 0', padding: '0' }}>
         <h1>My Missions</h1>
       </span>
       <ul>
         {joined.length > 0 ? (
           joined.map((mission) => (
-            <table className="profile_table_container" border={1} style={table} key={mission.id}>
-              <tr>
-                <td>
-                  <p>{mission.mission_name}</p>
-                </td>
-              </tr>
+            <table border={1} style={table} key={mission.mission_id}>
+              <tbody>
+                <tr>
+                  <td>
+                    <p>{mission.mission_name}</p>
+                  </td>
+                </tr>
+              </tbody>
             </table>
           ))
         ) : (
