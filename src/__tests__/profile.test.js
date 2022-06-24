7a871b5fa88fe5553066cca0d50490624923d6f1
yosefHesham <yousefmobdev@gmail.com>
2022-06-24 22:19:41 +0200

    Fix js linter errors

--- src/__tests__/profile.test.js
+++ src/__tests__/profile.test.js
@@ -1,33 +1,31 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import JoinedMissions from "../components/profile/joinedMissions";
-import Profile from "../components/profile/profile";
-import ReservedRockets from "../components/profile/reservedRockets";
-import store from "../redux/configure_store";
-import { Provider } from "react-redux";
-import Rockets from "../components/rockets/rockets";
-import Missions from "../components/missions/missions";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import JoinedMissions from '../components/profile/joinedMissions';
+import Profile from '../components/profile/profile';
+import ReservedRockets from '../components/profile/reservedRockets';
+import store from '../redux/configure_store';
+import Rockets from '../components/rockets/rockets';
+import Missions from '../components/missions/missions';
 
-const createComponent = (component) => {
-  return <Provider store={store}>{component}</Provider>;
-};
+const createComponent = (component) => <Provider store={store}>{component}</Provider>;
 
-describe("Test for components", () => {
-  test("Profile page should render correctly", () => {
+describe('Test for components', () => {
+  test('Profile page should render correctly', () => {
     const component = renderer
-      .create(createComponent(<Profile></Profile>))
+      .create(createComponent(<Profile />))
       .toJSON();
 
     expect(component).toMatchSnapshot();
   });
-  test("ReservedRockets should render correctly", () => {
+  test('ReservedRockets should render correctly', () => {
     const component = renderer
       .create(createComponent(<ReservedRockets />))
       .toJSON();
 
     expect(component).toMatchSnapshot();
   });
-  test("joinedMissions  should render correctly", () => {
+  test('joinedMissions  should render correctly', () => {
     const component = renderer
       .create(createComponent(<JoinedMissions />))
       .toJSON();
@@ -35,12 +33,12 @@ describe("Test for components", () => {
     expect(component).toMatchSnapshot();
   });
 
-  test("rocket page should render properly", () => {
+  test('rocket page should render properly', () => {
     const component = renderer.create(createComponent(<Rockets />)).toJSON();
 
     expect(component).toMatchSnapshot();
   });
-  test("Missions page should render properly", () => {
+  test('Missions page should render properly', () => {
     const component = renderer.create(createComponent(<Missions />)).toJSON();
 
     expect(component).toMatchSnapshot();
