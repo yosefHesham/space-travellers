035bc22448e2bea11d472ae85aba08e19cd571e6
yosefHesham <yousefmobdev@gmail.com>
2022-06-24 21:51:21 +0200

    Fix js linter errors

--- src/__tests__/action_creators.test.js
+++ src/__tests__/action_creators.test.js
@@ -1,21 +1,21 @@
-import { changeMissionStats, loadMissions } from "../redux/missions/missions";
+import { changeMissionStats, loadMissions } from '../redux/missions/missions';
 
-describe("Test missions action creators", () => {
-  test("Test get missions should return proper action type", () => {
+describe('Test missions action creators', () => {
+  test('Test get missions should return proper action type', () => {
     // arrange
-    const missions = ["mission1", "mision2"];
+    const missions = ['mission1', 'mision2'];
     const action = {
-      type: "LOAD_MISSIONS",
+      type: 'LOAD_MISSIONS',
       missions,
     };
 
     expect(loadMissions(missions)).toStrictEqual(action);
   });
-  test("Test get missions should return proper action type", () => {
+  test('Test get missions should return proper action type', () => {
     // arrange
-    const id = "1";
+    const id = '1';
     const action = {
-      type: "CHANGE_MISSION_STATUS",
+      type: 'CHANGE_MISSION_STATUS',
       payLoad: id,
     };
 
--- src/__tests__/reducers.test.js
+++ src/__tests__/reducers.test.js
@@ -1,55 +1,53 @@
 import missionsReducer, {
   changeMissionStats,
   loadMissions,
-} from "../redux/missions/missions";
-import rocketsReducer from "../redux/rockets/rockets";
+} from '../redux/missions/missions';
+import rocketsReducer from '../redux/rockets/rockets';
 
-describe("Test Missions reducer", () => {
-  test("Should return inital state", () => {
+describe('Test Missions reducer', () => {
+  test('Should return inital state', () => {
     expect(missionsReducer(undefined, {})).toStrictEqual([]);
   });
 
-  test("Should return proper payload", () => {
+  test('Should return proper payload', () => {
     // arrange
-    const missions = ["mission1", "mission2"];
+    const missions = ['mission1', 'mission2'];
     expect(missionsReducer(undefined, loadMissions(missions))).toStrictEqual(
-      missions
+      missions,
     );
   });
 
-  test("Should change reserved status", () => {
-    //arrange
+  test('Should change reserved status', () => {
+    // arrange
     const missions = [
       {
-        mission_name: "mission1",
-        mission_id: "1",
+        mission_name: 'mission1',
+        mission_id: '1',
         reserved: false,
       },
     ];
     const updated = [
       {
-        mission_name: "mission1",
-        mission_id: "1",
+        mission_name: 'mission1',
+        mission_id: '1',
         reserved: true,
       },
     ];
-    expect(missionsReducer(missions, changeMissionStats("1"))).toStrictEqual(
-      updated
+    expect(missionsReducer(missions, changeMissionStats('1'))).toStrictEqual(
+      updated,
     );
   });
 });
 
-
-describe("Test rockets reducers",() => {
-  test("It should create intial state",() => {
-
-    expect(rocketsReducer(undefined,{})).toStrictEqual([])
-  })
-  test("It should return list of rockets",() => {
-    const rockets = ["rocket1","rocket2"];
-    expect(rocketsReducer([],{
-      "type":"GET_ROCKETS",
-      payload:rockets
-    })).toStrictEqual(rockets)
-  })
-})
+describe('Test rockets reducers', () => {
+  test('It should create intial state', () => {
+    expect(rocketsReducer(undefined, {})).toStrictEqual([]);
+  });
+  test('It should return list of rockets', () => {
+    const rockets = ['rocket1', 'rocket2'];
+    expect(rocketsReducer([], {
+      type: 'GET_ROCKETS',
+      payload: rockets,
+    })).toStrictEqual(rockets);
+  });
+});
